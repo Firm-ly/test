a29931823b39b78c57dd08b188747f4fb8b14ebf
Firm-ly <15872092466@163.com>
2022-07-13 10:22:08 +0800

    second version

--- test.java
+++ test.java
@@ -1,4 +1,10 @@
 
-public static void main(String args[]){
-  System.out.println("This is a main method")
+public class Test{
+   public static void main(String args[]){
+      System.out.println("This is a main method");
+      add(1,2);
+   }
+   public int add (int a int b){
+      return a+b;
+   } 	
 }
